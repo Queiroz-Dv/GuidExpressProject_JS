c61d68bc906e5e9af0515849fe3d86c6b42d262e
Queiroz <kalifor_dbel@hotmail.com>
2022-02-28 18:24:02 -0300

    Add categories form

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,7 +1,26 @@
+const { request } = require("express");
 const express = require("express");
 const router = express.Router(); //Create Route Page
+const Category = require("./Category");
+const slugify = require("slugify");
 
-router.get("/categories", (request, response) => {
-  response.send();
+//Create New Category
+router.get("/admin/categories/new", (request, response) => {
+  response.render("admin/categories/new");;
+});
+
+//Add new category in the database
+router.post("/categories/save", (request, response)=>{
+  var title = request.body.title;
+  if (title!=undefined) {
+    Category.create({
+      title:title,
+      slug: slugify(title)
+    }).then(()=>{
+      response.redirect("/");
+    })
+  } else {
+    response.redirect("/admin/categories/new");
+  }
 })
 module.exports = router;
