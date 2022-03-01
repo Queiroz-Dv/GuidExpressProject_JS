ce1332ed451c2d3d0a566e17b8bd829c8b5fa4a9
Queiroz <kalifor_dbel@hotmail.com>
2022-03-01 12:23:44 -0300

    Add article's route

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router(); //Create Route Page
+const Category = require("../categories/Category");
 
-router.get("/articles", (request, response) => {
-  response.send();
-})
+//Routes
+router.get("/admin/articles/new", (request, response) => {
+  Category.findAll().then(categories=>{
+    response.render("admin/articles/new", {categories: categories})
+  })
+  response.render("admin/articles/new");
+});
 module.exports = router;
